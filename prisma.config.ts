99956a72d153f74b301120161db30dec1696d260
iLee66 <ileellcrealty@gmail.com>
2026-09-01 17:55:05 -0400

    Remove datasource configuration from prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,7 +3,4 @@ import { defineConfig } from "prisma/config";
 
 export default defineConfig({
     schema: "prisma/schema.prisma",
-    datasource: {
-        url: "file:./prisma/dev.db",
-    },
 });
